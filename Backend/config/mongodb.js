85189593962a89d0c6d4869c70f79a11d28e3362
Ragavikrishnan <ragavikrishnan03@gmail.com>
2026-06-06 22:40:20 +0530

    Remove file

--- Backend/config/mongodb.js
+++ Backend/config/mongodb.js
@@ -7,3 +7,16 @@ const connect=async()=>{
 await mongoose.connect(process.env.MONGODB_URL);
 }
 export default connect;
+
+
+const mongoose=require('mongoose');
+
+const connect=async()=>{
+    mongoose.connection.on('connected',()=>{
+        console.log('mongoose connected');
+
+    })
+    await mongoose.connect(process.env.MONGODB_URL)
+}
+
+module.export=connect
